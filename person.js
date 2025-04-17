c3d4460ea1170b588d0a31796c466ee0173f51ec
Fred <barillondev@gmail.com>
2025-04-17 11:55:05 +0200

    first commit

---
+++ person.js
@@ -0,0 +1,10 @@
+// person.js
+
+export default class Person {
+  constructor(name, date, time, participants) {
+    this.name = name;
+    this.date = date;
+    this.time = time;
+    this.participants = participants;
+  }
+}
