7404ef6e5aaf0ace9ddaaba8f61d03b2d49c34a2
Fredbarillon <134544114+Fredbarillon@users.noreply.github.com>
2025-07-07 10:34:22 +0200

    Delete IPen.java

--- IPen.java
+++
@@ -1,9 +0,0 @@
-package zoo.Interface;
-
-import java.util.List;
-import zoo.Class.Animal;
-
-public interface IPen<T extends Animal> {
-    String addAnimal(T animal);
-    List<T> displayAnimals();
-}
