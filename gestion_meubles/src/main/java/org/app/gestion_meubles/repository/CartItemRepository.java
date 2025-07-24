31e4aef6fa68e560e8d4f14f921fa2350c6e05f0
Fredbarillon <134544114+Fredbarillon@users.noreply.github.com>
2025-07-24 14:59:34 +0200

    Delete gestion_meubles directory

--- gestion_meubles/src/main/java/org/app/gestion_meubles/repository/CartItemRepository.java
+++
@@ -1,9 +0,0 @@
-package org.app.gestion_meubles.repository;
-
-import org.app.gestion_meubles.model.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
-}
--- gestion_meubles/src/main/java/org/app/gestion_meubles/repository/FurnitureRepository.java
+++
@@ -1,9 +0,0 @@
-package org.app.gestion_meubles.repository;
-
-import org.app.gestion_meubles.model.entity.Furniture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface FurnitureRepository extends JpaRepository<Furniture, UUID> {
-}
