a04ca1ee4a66f7ff9ae237b479bb90ba971d3c2d
Fredbarillon <134544114+Fredbarillon@users.noreply.github.com>
2025-08-01 09:41:19 +0200

    Delete tpDocker directory

--- tpDocker/src/main/java/org/app/tpdocker/model/dto/FartDTO.java
+++
@@ -1,3 +0,0 @@
-package org.app.tpdocker.model.dto;
-
-public record FartDTO( Long id, String smell, int dissipation, double soundVolume, String origin)  {}
--- tpDocker/src/main/java/org/app/tpdocker/repository/FartRepository.java
+++
@@ -1,7 +0,0 @@
-package org.app.tpdocker.repository;
-
-import org.app.tpdocker.model.entity.Fart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FartRepository extends JpaRepository<Fart, Long> {
-}
