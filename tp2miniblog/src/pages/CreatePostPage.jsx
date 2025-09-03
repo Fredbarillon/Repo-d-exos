dc6bc8d22022a431bf7e2ad6be7bc8cc78971d86
Fredbarillon <134544114+Fredbarillon@users.noreply.github.com>
2025-09-03 16:32:28 +0200

    Delete tp2miniblog directory

--- tp2miniblog/src/pages/CreatePostPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function CreatePost() {
-    return (
-        <div>
-            <h2>Create Post Page</h2>
-            <p>This is where the form to create a new post will be displayed.</p>
-        </div>
-    );
-}
--- tp2miniblog/src/pages/PostsDetailPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function PostsDetailPage() {
-    return (
-        <div>
-            <h2>Post Detail Page</h2>
-            <p>This is where the details of a single post will be displayed.</p>
-        </div>
-    );
-}
