0e8fa65537e2e1d4ac09b33409de34434239291d
Fredbarillon <134544114+Fredbarillon@users.noreply.github.com>
2025-09-03 16:10:25 +0200

    list done

---
+++ tp2miniblog/src/pages/PostsDetailPage.jsx
@@ -0,0 +1,8 @@
+export default function PostsDetailPage() {
+    return (
+        <div>
+            <h2>Post Detail Page</h2>
+            <p>This is where the details of a single post will be displayed.</p>
+        </div>
+    );
+}
